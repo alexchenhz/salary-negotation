3c87475a8f0fdc06890c667b9990fe13a037583a
Chen Alex <ahc49@zoo.cs.yale.edu>
2022-12-14 14:20:40 -0500

    Must use ray 2.1.0

--- job_search.py
+++ job_search.py
@@ -2,15 +2,13 @@ import argparse
 import os
 
 import ray
-from gym.spaces import Box, Discrete
-from ray import air, tune
-from ray.rllib.algorithms import ppo
-from ray.rllib.algorithms.ppo import PPOConfig
+from ray import tune
+from ray.rllib.agents.ppo import ppo
+from ray.rllib.algorithms.ppo import PPOConfig, PPOTorchPolicy
 from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from ray.rllib.models import ModelCatalog
-from ray.tune.logger import pretty_print
+from ray.rllib.policy.policy import PolicySpec
 from ray.tune.registry import register_env
-from ray.rllib.agents.ppo import ppo
 
 import environment.job_search_environment as job_search_env
 from models.job_search_model import JobSearchModelV0
@@ -19,16 +17,16 @@ from models.job_search_model import JobSearchModelV0
 def get_cli_args():
     """Create CLI parser and return parsed arguments"""
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument(
         "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
     )
     parser.add_argument("--num-cpus", type=int, default=0)
-    
+
     parser.add_argument("--num-gpus", type=int, default=0)
 
     parser.add_argument("--eager-tracing", action="store_true")
-    
+
     parser.add_argument(
         "--stop-iters", type=int, default=5, help="Number of iterations to train."
     )
@@ -39,16 +37,16 @@ def get_cli_args():
         help="Number of timesteps to train.",
     )
     parser.add_argument(
+        "--checkpoint-freq",
+        type=int,
+        default=1,
+        help="Number of iterations between checkpoint saves"
+    )
+    parser.add_argument(
         "--stop-reward",
         type=float,
         default=1000,
         help="Reward at which we stop training.",
-    )
-    parser.add_argument(
-        "--no-tune",
-        action="store_true",
-        help="Run without Tune using a manual train loop instead. Here,"
-        "there is no TensorBoard support.",
     )
     parser.add_argument(
         "--local-mode",
@@ -59,7 +57,7 @@ def get_cli_args():
         "--num-samples",
         type=int,
         default=1,
-        help="Number of samples to run concurrently"
+        help="Number of samples to run concurrently",
     )
 
     args = parser.parse_args()
@@ -80,8 +78,41 @@ if __name__ == "__main__":
     env_name = "job_search_env"
     register_env(env_name, lambda config: ParallelPettingZooEnv(env_creator(config)))
 
-    # Register the model
-    ModelCatalog.register_custom_model("JobSearchModelV0", JobSearchModelV0)
+    # Get observation space and action space from environment (all agents should have the same spaces)
+    obs_space = job_search_env.env().observation_space("candidate_0")
+    act_space = job_search_env.env().action_space("candidate_0")
+
+    # Register the models
+    ModelCatalog.register_custom_model("CandidateModel", JobSearchModelV0)
+    ModelCatalog.register_custom_model("EmployerModel", JobSearchModelV0)
+
+    policies = {
+        "candidate_policy": (
+            PPOTorchPolicy,
+            obs_space,
+            act_space,
+            {
+                "model": {
+                    "custom_model": "CandidateModel",
+                    "custom_model_config": {},
+                }
+            },
+        ),
+        "employer_policy": (
+            PPOTorchPolicy,
+            obs_space,
+            act_space,
+            {
+                "model": {
+                    "custom_model": "EmployerModel",
+                    "custom_model_config": {},
+                }
+            },
+        )
+    }
+
+    def policy_mapping_fn(agent_id, episode, worker, **kwargs):
+        return "candidate_policy" if "candidate" in agent_id else "employer_policy"
 
     config = (
         PPOConfig()
@@ -95,11 +126,12 @@ if __name__ == "__main__":
                 "custom_model_config": {},
             }
         )
+        # .multi_agent(policies=policies, policy_mapping_fn=policy_mapping_fn)
     ).to_dict()
 
     # Set observation space and action space (all agents should have the same spaces)
-    config["observation_space"] = job_search_env.env().observation_space("candidate_0")
-    config["action_space"] = job_search_env.env().action_space("candidate_0")
+    config["observation_space"] = obs_space
+    config["action_space"] = act_space
 
     stop = {
         "training_iteration": args.stop_iters,
@@ -110,7 +142,7 @@ if __name__ == "__main__":
     tune.run(
         ppo.PPOTrainer,
         stop=stop,
-        checkpoint_freq=5,
+        checkpoint_freq=args.checkpoint_freq,
         local_dir="./ray_results/" + env_name,
         config=config,
         num_samples=args.num_samples,
