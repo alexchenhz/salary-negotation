41e39a5113b50d0e4aaa6ee94f1e98d4ac6d0bb1
Chen Alex <ahc49@zoo.cs.yale.edu>
2022-12-14 16:11:31 -0500

    create simulation to show results

---
+++ job_search_simulation.py
@@ -0,0 +1,32 @@
+
+from ray.rllib.agents.ppo import ppo
+
+class JobSearchSimulation():
+    
+    def load(self, path):
+        """
+        Load a trained RLlib agent from the specified path. Call this before testing a trained agent.
+        :param path: Path pointing to the agent's saved checkpoint (only used for RLlib agents)
+        """
+        self.agent = ppo.PPOTrainer(config=self.config, env=self.env_class)
+        self.agent.restore(path)
+
+    def test(self):
+        """Test trained agent for a single episode. Return the episode reward"""
+        # instantiate env class
+        env = self.env_class(self.env_config)
+
+        # run until episode ends
+        episode_reward = 0
+        done = False
+        obs = env.reset()
+        while not done:
+            action = self.agent.compute_action(obs)
+            obs, reward, done, info = env.step(action)
+            episode_reward += reward
+
+        return episode_reward
+    
+if __name__ == "__main__":
+    pass
+    
